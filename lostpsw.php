b9f852d9e9ad8c5878f9b21f59d817ea2c644732
Xande Brum <2115353@talnet.nl>
2020-09-10 20:38:16 +0200

    lost password page

---
+++ lostpsw.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="lostpsw.php" >
+
+      <input type="text" name="lostpasw" placeholder="E-mail"required>
+      <br>
+      <button type="submit">submit</button>
+
+
+    </form>
+
+
+  </body>
+</html>
