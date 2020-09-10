93e51cde0d8debeded3f1be4f217092d0cdd8fd1
Xande Brum <2115353@talnet.nl>
2020-09-10 20:37:51 +0200

    login page

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+
+    <form action="index.php">
+
+          usename: <input type="text" name= "username" placeholder="username" required>
+        <br>
+          password: <input type="password" name= "password" placeholder="password" required>
+        <br>
+          <button type="submit">Login</button>
+        <br>
+          <a href="signup.php">account maken</a>
+          <a href="lostpsw.php">password forgotten?</a>
+
+    </form>
+
+</body>
+
+
+</html>
