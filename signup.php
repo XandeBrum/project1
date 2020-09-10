dccf69d8fc9aaeb97f9fae0f11dc80a8465ffc83
Xande Brum <2115353@talnet.nl>
2020-09-10 20:38:50 +0200

    sign up page

---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="signup.php">
+
+      <input type="text" name="voornaam" placeholder="firstname" required/>
+      <input type="text" name="tussenvoegsel" placeholder="tussenvoegsel" />
+      <input type="text" name="achternaam" placeholder="lastname" required/>
+      <input type="text" name="email" placeholder="E-mail" required/>
+      <input type="text" name="gebruikersnaam" placeholder="usename" required/>
+      <input type="password" name="wachtwoord" placeholder="password" required />
+      <input type="password" name="herhaal_wachtwoord" placeholder="repeat password" required />
+      <button type="submit">Register</button>
+
+    </form>
+
+
+
+
+  </body>
+</html>
